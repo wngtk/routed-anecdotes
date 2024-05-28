75e6e2c947e8d60cfa38015b3c468c587ece55d0
Wang Tiaoke <100271394+wngtk@users.noreply.github.com>
2024-05-28 19:01:01 +0800

    7.6: Anecdotes and Hooks step 3

--- src/App.jsx
+++ src/App.jsx
@@ -57,9 +57,9 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
-  const content = useField('text')
-  const author = useField('text')
-  const info = useField('text')
+  const {reset: resetContent, ...content} = useField('text')
+  const {reset: resetAuthor, ...author} = useField('text')
+  const {reset: resetInfo, ...info} = useField('text')
 
   const navigate = useNavigate()
 
@@ -76,9 +76,9 @@ const CreateNew = (props) => {
 
   const onReset = (e) => {
     e.preventDefault()
-    content.reset()
-    author.reset()
-    info.reset()
+    resetContent()
+    resetAuthor()
+    resetInfo()
   }
 
   return (
